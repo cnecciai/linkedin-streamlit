c45991b682f610f67b910afcaa1341a2471afa4a
cnecciai <clarknecciai2@gmail.com>
2023-12-11 11:23:53 -0500

    Commenting Progress Bar

--- streamlit-app.py
+++ streamlit-app.py
@@ -195,9 +195,9 @@ with col_p:
     #Show output 
     if user_make_prediction == True: 
 
+        #Progress Bar - Additional Interactivity
         progress_text = "Making a prediction. Please wait."
         my_bar = st.progress(0, text=progress_text)
-            
         for percent_complete in range(100):
             time.sleep(0.01)
             my_bar.progress(percent_complete + 1, text=progress_text)
